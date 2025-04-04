e0c754bc63a8ed3dcf94d8659d405d1160844e59
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-04-04 17:03:28 -0300

    Início do projeto de Gestão de Equipamentos

---
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -0,0 +1,20 @@
+﻿namespace GestaoDeEquipamentos.ConsoleApp
+{
+    public class Equipamento
+    {
+        public int Id;
+        public string Nome;
+        public string Fabricante;
+        public decimal PrecoAquisicao;
+        public DateTime DataFabricacao;
+
+
+        public string ObterNumeroSerie()
+        {
+            string tresPrimeirosCaracteres = Nome.Substring(0, 3).ToUpper();
+            return $"{tresPrimeirosCaracteres}-{Id}";
+        }
+    }
+}
+
+
--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Equipamento equipamentoUm = new Equipamento();
+            equipamentoUm.Id = 0001;
+            equipamentoUm.Nome = "Laptop";
+            equipamentoUm.Fabricante = "Asus";
+            equipamentoUm.PrecoAquisicao = 3800;
+            equipamentoUm.DataFabricacao = new DateTime(2024,08,11);
+
+            Console.WriteLine(equipamentoUm.Id);
+            Console.WriteLine(equipamentoUm.Nome);
+            Console.WriteLine(equipamentoUm.Fabricante);
+            Console.WriteLine(equipamentoUm.PrecoAquisicao);
+            Console.WriteLine(equipamentoUm.DataFabricacao);
+            Console.WriteLine(equipamentoUm.ObterNumeroSerie());
+
+
+            Console.ReadLine();
         }
     }
 }
